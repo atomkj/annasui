65a0e580512d82ac17d8a1ce22d2ae1f1930370e
atomkj <80096082+atomkj@users.noreply.github.com>
2021-04-19 17:45:24 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//슬릭구현
+
+$(document).ready(function(){
+    //mainSlider
+    $('.mainImg').slick({
+        dots:true,
+        prevArrow:'<div class="prevBtn">이전</div>',
+        nextArrow:'<div class="nextBtn">다음</div>',
+        autoplay:true,
+        autoplaySpeed:5000,
+    })
+    
+   
+    
+    //mainCon01
+    $('.con01Menu').slick({
+     slidesToShow:3,
+     slidesToScroll:1,
+     dots:false,  //네모난 버튼 없애기
+     prevArrow:$('.prevBtn01'),
+     nextArrow:$('.nextBtn01'),
+ });
+    //mainCon05
+    $('.con05Img').slick({
+        slidesToShow:2,
+        slidesToScroll:2,
+        dots:false,  //네모난 버튼 없애기
+        prevArrow:$('.prevBtn05'),
+        nextArrow:$('.nextBtn05'),
+    })
+   
+    //wow
+    new WOW().init();
+})
+
+ $(window).scroll(function(){
+        var sc_top=$(this).scrollTop();  //scrollTop이 var sc_top변환됨,위에 $(window)=$(this) 같은내용
+      
+         if(sc_top>900){
+            $('.snsIcon').addClass('fixed');
+        }else if(sc_top<900){
+            $('.snsIcon').removeClass('fixed');
+        }
+    })
